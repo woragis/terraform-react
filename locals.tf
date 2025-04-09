9f902218568cb7869c6269f1c75bd6774c3b934b
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-09 15:40:29 -0300

    Remove commments

--- locals.tf
+++ locals.tf
@@ -10,17 +10,17 @@ locals {
 
   # CloudFront and Certificate Names
   cloudfront_name       = var.cloudfront_distribution_name != null ? var.cloudfront_distribution_name : "${local.normalized_project}-${local.environment}-cdn"
-  certificate_name      = "${local.normalized_project}-${local.environment}-cert"  # No variable override for this, keeping as is
+  certificate_name      = "${local.normalized_project}-${local.environment}-cert"
   cert_validation       = tolist(aws_acm_certificate.cert.domain_validation_options)[0]
 
   # Tags
   common_tags           = {
     Project     = var.project_name
-    Environment = local.environment  # Use normalized environment
+    Environment = local.environment
     ManagedBy   = "Terraform"
   }
 
   # CloudFront Settings
-  ssl_minimum_protocol_version = var.ssl_minimum_protocol_version  # Use variable instead of hardcoding
+  ssl_minimum_protocol_version = var.ssl_minimum_protocol_version
   cloudfront_origin_id         = "${local.normalized_project}-s3-origin"
 }
