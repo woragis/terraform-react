d0c88e492fc70ac29613e8e9475c015e1c3838f5
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-09 21:21:30 -0300

    Update null variables logic

--- locals.tf
+++ locals.tf
@@ -1,21 +1,21 @@
 locals {
   # Domain and Naming
   full_domain        = "${var.subdomain}.${var.root_domain}"
-  normalized_project = trim(var.project_name) != "" ? lower(var.project_name) : "app"
+  normalized_project = trim(var.project_name, " ") != "" ? lower(var.project_name) : "app"
   environment        = lower(var.environment)
 
   # S3 Bucket Names
-  s3_bucket_name      = trim(var.s3_bucket_name) != "" ? var.s3_bucket_name : "${local.normalized_project}-${local.environment}-static"
-  logging_bucket_name = trim(var.logging_bucket_name) != "" ? var.logging_bucket_name : "${local.normalized_project}-${local.environment}-logs"
+  s3_bucket_name      = trim(var.s3_bucket_name, " ") != "" ? var.s3_bucket_name : "${local.normalized_project}-${local.environment}-static"
+  logging_bucket_name = trim(var.logging_bucket_name, " ") != "" ? var.logging_bucket_name : "${local.normalized_project}-${local.environment}-logs"
 
   # CloudFront and Certificate Names
-  cloudfront_name = trim(var.cloudfront_distribution_name) != "" ? var.cloudfront_distribution_name : "${local.normalized_project}-${local.environment}-cdn"
+  cloudfront_name  = trim(var.cloudfront_distribution_name, " ") != "" ? var.cloudfront_distribution_name : "${local.normalized_project}-${local.environment}-cdn"
   certificate_name = "${local.normalized_project}-${local.environment}-cert"
   cert_validation  = tolist(aws_acm_certificate.cert.domain_validation_options)[0]
 
   # Tags
   common_tags = {
-    Project     = trim(var.project_name) != "" ? var.project_name : "App"
+    Project     = trim(var.project_name, " ") != "" ? var.project_name : "App"
     Environment = local.environment
     ManagedBy   = "Terraform"
     Purpose     = "ReactFrontend"
