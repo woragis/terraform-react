29fe896e4f263d2f0aa606ffa0ef0dd836abe93c
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-09 21:08:56 -0300

    Update action defaults

--- locals.tf
+++ locals.tf
@@ -1,24 +1,24 @@
 locals {
   # Domain and Naming
-  full_domain           = "${var.subdomain}.${var.root_domain}"
-  normalized_project    = var.project_name != null ? lower(var.project_name) : "app"  # Fallback to "app" if null
-  environment           = lower(var.environment)
+  full_domain        = "${var.subdomain}.${var.root_domain}"
+  normalized_project = trim(var.project_name) != "" ? lower(var.project_name) : "app"
+  environment        = lower(var.environment)
 
   # S3 Bucket Names
-  s3_bucket_name        = var.s3_bucket_name != null ? var.s3_bucket_name : "${local.normalized_project}-${local.environment}-static"
-  logging_bucket_name   = var.logging_bucket_name != null ? var.logging_bucket_name : "${local.normalized_project}-${local.environment}-logs"
+  s3_bucket_name      = trim(var.s3_bucket_name) != "" ? var.s3_bucket_name : "${local.normalized_project}-${local.environment}-static"
+  logging_bucket_name = trim(var.logging_bucket_name) != "" ? var.logging_bucket_name : "${local.normalized_project}-${local.environment}-logs"
 
   # CloudFront and Certificate Names
-  cloudfront_name       = var.cloudfront_distribution_name != null ? var.cloudfront_distribution_name : "${local.normalized_project}-${local.environment}-cdn"
-  certificate_name      = "${local.normalized_project}-${local.environment}-cert"
-  cert_validation       = tolist(aws_acm_certificate.cert.domain_validation_options)[0]
+  cloudfront_name = trim(var.cloudfront_distribution_name) != "" ? var.cloudfront_distribution_name : "${local.normalized_project}-${local.environment}-cdn"
+  certificate_name = "${local.normalized_project}-${local.environment}-cert"
+  cert_validation  = tolist(aws_acm_certificate.cert.domain_validation_options)[0]
 
   # Tags
-  common_tags           = {
-    Project     = var.project_name != null ? var.project_name : "App"  # Fallback to "App" if null
+  common_tags = {
+    Project     = trim(var.project_name) != "" ? var.project_name : "App"
     Environment = local.environment
     ManagedBy   = "Terraform"
-    Purpose     = "ReactFrontend" # or "Infrastructure", "Monitoring", etc
+    Purpose     = "ReactFrontend"
   }
 
   # CloudFront Settings
