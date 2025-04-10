8cb8164a9078eb7fac7c4cab2f9768056aeb92a9
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-10 03:01:17 -0300

    Update terraform import variables

--- locals.tf
+++ locals.tf
@@ -5,6 +5,6 @@ locals {
     ManagedBy   = "Terraform"
   }
 
-  s3_bucket_name       = "${var.project_name}-${var.environment}-static-site"
+  s3_bucket_name       = "${var.project_name}-${var.environment}-s3"
   logging_bucket_name  = "${var.project_name}-${var.environment}-logs"
 }
